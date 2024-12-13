04578f2d2b279426ce04349967d9e0e3f79dee52
Andrew Sorem <andrewsorem9@gmail.com>
2024-12-13 15:17:11 -0800

    CompletedLab07

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,6 +1,9 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
 import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;              //class for GUI window
 import javafx.scene.Scene;              //class for specific view in GUI window
 import javafx.scene.layout.VBox;        //class for layout pane, organized top-to-bottom
@@ -9,18 +12,64 @@ import javafx.scene.control.Button;     //class for button component
 import javafx.event.EventHandler;       //interface for handling events
 import javafx.event.ActionEvent;        //class for type of event for action (like button or key pressed)
 
-public class HelloApplication extends Application  { //inheriting core functionality + this class will handle events
-    /*** GUI COMPONENTS ***/
-    // TODO: follow step 25 in README.md to create reference variables
+public class HelloApplication extends Application implements EventHandler<ActionEvent>  { //inheriting core functionality + this class will handle events
 
     /*** DRIVER main ***/
     public static void main(String[] args) {
         launch(args); //method from Application class, must be called to setup javafx application
     }
 
-    // TODO: follow steps 2-9 in README.md to create a start method
+    Button button1, button2;
+    Label label1, label2;
+    TextField textField;
 
-    // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello GUI: Andrew Sorem");
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout, 300, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
-    // TODO: follow steps 22-34 in README.md to create an event handler
+        AnchorPane anchorPane = new AnchorPane();
+
+        label1 = new Label("Hello GUI World");
+        anchorPane.setTopAnchor(label1, 30.0);
+        anchorPane.setRightAnchor(label1, 30.0);
+        anchorPane.getChildren().add(label1);
+
+        label2 = new Label("Hello World");
+        anchorPane.setTopAnchor(label2, 30.0);
+        anchorPane.setLeftAnchor(label2, 30.0);
+        anchorPane.getChildren().add(label2);
+        layout.getChildren().add(anchorPane);
+
+        textField = new TextField();
+        textField.setPromptText("Enter text here");
+        anchorPane.setTopAnchor(textField, 150.0);
+        anchorPane.setLeftAnchor(textField, 75.0);
+        anchorPane.setRightAnchor(textField, 75.0);
+        anchorPane.getChildren().add(textField);
+
+        button1 = new Button("Change Label Text");
+        anchorPane.setBottomAnchor(button1, 30.0);
+        anchorPane.setRightAnchor(button1, 30.0);
+        anchorPane.getChildren().add(button1);
+        button1.setOnAction(this);
+
+        button2 = new Button("Laugh");
+        anchorPane.setBottomAnchor(button2, 30.0);
+        anchorPane.setLeftAnchor(button2, 30.0);
+        anchorPane.getChildren().add(button2);
+        button2.setOnAction(this);
+    }
+
+    public void handle(ActionEvent e) {
+        if (e.getSource() == button1) {
+            String enteredText = textField.getText(); // Get the text from the TextField
+            label1.setText(enteredText);
+        }
+        if (e.getSource() == button2) {
+            label2.setText("HEHEHEHAW!!");
+        }
+    }
 }
